4dc5d7272cf50ebcb448f9d5a272fa5ab14be9d7
Muhammad.Elzeiny <Muhammad.Elzeiny@AVE-LAP-270.AVELABS.CAI>
2019-02-28 20:38:02 +0200

    init LCD

---
+++ HAL/LCD/LCD.h
@@ -0,0 +1,13 @@
+/*
+ * LCD.h
+ *
+ *  Created on: Feb 28, 2019
+ *      Author: Muhammad.Elzeiny
+ */
+
+#ifndef J7_LAB_SRC_HAL_LCD_LCD_H_
+#define J7_LAB_SRC_HAL_LCD_LCD_H_
+
+
+
+#endif /* J7_LAB_SRC_HAL_LCD_LCD_H_ */
