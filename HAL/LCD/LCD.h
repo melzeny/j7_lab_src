8b18f4e11ae809f864fc0e31c29d2fbcd8b779f6
muhammadElzeiny <muhammad.elzeiny@gmail.com>
2019-03-08 12:07:31 +0200

    ..

--- HAL/LCD/LCD.h
+++ HAL/LCD/LCD.h
@@ -8,5 +8,13 @@
 #ifndef J7_LAB_SRC_HAL_LCD_LCD_H_
 #define J7_LAB_SRC_HAL_LCD_LCD_H_
 
+/*Commands*/
+#define LCD_CMD_CLEAR		(u8)0x01
+
+/*APIs*/
+void LCD_init(void);
+void LCD_writeChar(u8 Data);
+void LCD_writeCmd(u8 Cmd);
+
 
 #endif /* J7_LAB_SRC_HAL_LCD_LCD_H_ */
