07516f35ad834bf6d705861be98799b3c73db3f7
Muhammad.Elzeiny <Muhammad.Elzeiny@AVE-LAP-270.AVELABS.CAI>
2019-03-22 13:48:50 +0200

    modify timer2 fast pwm and phase correct

---
+++ APP_LABS/LAB_08_FastPwm.c
@@ -0,0 +1,19 @@
+/*
+ * LAB_08_FastPwm.c
+ *
+ *  Created on: Mar 22, 2019
+ *      Author: Muhammad.Elzeiny
+ */
+#include "../MCAL/DIO/DIO.h"
+#include "../MCAL/TIMER_2/TIMER2.h"
+void LAB_08_FastPwm(void)
+{
+	DIO_init();
+	TIMER2_init();
+	while(1)
+	{
+
+	}
+
+}
+
